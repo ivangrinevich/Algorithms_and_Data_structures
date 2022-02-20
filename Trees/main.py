88ae24ed42d3f5b0469f197d3d2ac9bd856fb866
ivangrinevich <ivanushka20032502@gmail.com>
2022-02-20 23:19:42 +0300

    Individual_task

--- Trees/main.py
+++ Trees/main.py
@@ -171,7 +171,7 @@ def pre_order_traversal(v):
             queue.append(node.left)
 
 
-with open('in.txt', 'r', encoding='utf-8') as f_input, open('out.txt', 'w', encoding='utf-8') as f_output:
+with open('../inputs/in.txt', 'r', encoding='utf-8') as f_input, open('../outputs/out.txt', 'w', encoding='utf-8') as f_output:
     my_tree = Tree()
     tree = int(f_input.readline())
     insert(tree)
