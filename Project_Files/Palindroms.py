3b36e2159701a7212020487be777c3920507cee8
ivangrinevich <ivanushka20032502@gmail.com>
2022-03-19 23:34:43 +0300

    Palindroms Done

--- Project_Files/Palindroms.py
+++ Project_Files/Palindroms.py
@@ -1,26 +1,35 @@
+from array import array
 with open('../Individual_task/inputs/palindroms.txt', 'r', encoding='utf-8') as f_input, \
      open('../Individual_task/outputs/palindroms_out.txt', 'w', encoding='utf-8') as f_output:
-    n = f_input.readline().strip()
+    n = array('l')
+    n.fromlist([ord(i) for i in f_input.readline().strip()])
     k = len(n)
-    f = [[0 for _ in range(k)] for _ in range(k)]
-    for i in range(k):
-        f[i][i] = 1
-    for i in range(k - 1):
-        f[i][i + 1] = 2 if n[i] == n[i + 1] else 1
-    for j in range(2, k):
-        for i in range(k - j):
-            f[i][i + j] = f[i + 1][i + j - 1] + 2 if n[i] == n[i + j] else max(f[i + 1][i + j], f[i][i + j - 1])
-    i = 0
-    j = k - 1
-    sb = ''
-    while j >= i:
-        m = f[i][j]
-        while (i < k - 1) and (f[i + 1][j] == m):
+    if n == n[::-1]:
+        print(k, file=f_output)
+        print(n, file=f_output)
+    else:
+        pal = ''
+        f = [array('l', [0 for _ in range(k)]) for _ in range(k)]
+        for i in range(k):
+            f[i][i] = 1
+        for i in range(k - 1):
+            f[i][i + 1] = 2 if chr(n[i]) == chr(n[i + 1]) else 1
+        for z in range(k - 2):
+            j = z + 2
+            for i in range(k - j):
+                f[i][j] = f[i + 1][j - 1] + 2 if chr(n[i]) == chr(n[j]) else max(f[i + 1][j], f[i][j - 1])
+                j += 1
+        print(f[0][k - 1], file=f_output)
+        i = 0
+        j = k - 1
+        while j >= i:
+            m = f[i][j]
+            while (i < k - 1) and (f[i + 1][j] == m):
+                i += 1
+            while j > 0 and f[i][j - 1] == m:
+                j -= 1
             i += 1
-        while j > 0 and f[i][j - 1] == m:
+            pal += chr(n[j])
             j -= 1
-        i += 1
-        sb += (n[j])
-        j -= 1
-    print(str(f[0][k - 1]), file=f_output)
-    print(sb + sb[::-1][f[0][k - 1] % 2:], file=f_output)
+        print(pal + pal[::-1][f[0][k - 1] % 2:], file=f_output)
+
