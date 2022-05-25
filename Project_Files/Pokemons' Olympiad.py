8b2cee6f13f51fe3279b21cd97b805b8f51d2878
ivangrinevich <ivanushka20032502@gmail.com>
2022-05-25 13:45:52 +0300

    Olympiad Done

--- Project_Files/Pokemons' Olympiad.py
+++ Project_Files/Pokemons' Olympiad.py
@@ -11,25 +11,24 @@ for j in range(m - 1):
                 height_matrix[k][j + 1] = max(height_matrix[i - 1][j] + 1, height_matrix[k][j + 1])
             else:
                 height_matrix[k][j + 1] = max(height_matrix[k][j + 1], height_matrix[i - 1][j])
-height_matrix = list(zip(*height_matrix))
-if m not in height_matrix[m - 1]:
+if m not in list(zip(*height_matrix))[m - 1]:
     print(-1)
 else:
     positions = []
-    i = m - 1
-    j = n - 1
+    j = m - 1
+    i = n - 1
     k = 0
-    while i >= 0:
+    while j >= 0:
         if height_matrix[i][j] == m - k:
-            positions.append(j + 1)
-            if i == 0:
+            positions.append(i + 1)
+            if j == 0:
                 break
-            l = j
+            l = i
             k += 1
-            while pokemon_heights[i] + pedestal_heights[l] <= pokemon_heights[i - 1] + pedestal_heights[j - 1] or height_matrix[i - 1][j - 1] != m - k:
-                j -= 1
+            while pokemon_heights[j] + pedestal_heights[l] <= pokemon_heights[j - 1] + pedestal_heights[i - 1] or height_matrix[i - 1][j - 1] != m - k:
+                i -= 1
             i -= 1
             j -= 1
         else:
-            j -= 1
+            i -= 1
     print(*sorted(positions))
