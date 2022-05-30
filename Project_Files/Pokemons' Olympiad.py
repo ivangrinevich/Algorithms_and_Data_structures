e442c9c818919c789cf198f347ebe09046439ce5
ivangrinevich <ivanushka20032502@gmail.com>
2022-05-31 01:02:46 +0300

    Olympiad Done

--- Project_Files/Pokemons' Olympiad.py
+++ Project_Files/Pokemons' Olympiad.py
@@ -1,34 +1,41 @@
 m, n = [int(i) for i in input().split()]
 pokemon_heights = [int(i) for i in input().split()]
 pedestal_heights = [int(i) for i in input().split()]
-height_matrix = [[0 for j in range(m)] for i in range(n)]
-for i in range(n - m + 1):
-    height_matrix[i][0] = 1
-for j in range(m - 1):
-    for i in range(j + 1, n):
-        for k in range(i, n):
-            if pokemon_heights[j] + pedestal_heights[i - 1] < pokemon_heights[j + 1] + pedestal_heights[k]:
-                height_matrix[k][j + 1] = max(height_matrix[i - 1][j] + 1, height_matrix[k][j + 1])
-            else:
-                height_matrix[k][j + 1] = max(height_matrix[k][j + 1], height_matrix[i - 1][j])
-if m not in list(zip(*height_matrix))[m - 1]:
-    print(-1)
-else:
-    positions = []
-    j = m - 1
-    i = n - 1
-    k = 0
-    while j >= 0:
-        if height_matrix[i][j] == m - k:
-            positions.append(i + 1)
-            if j == 0:
-                break
-            l = i
-            k += 1
-            while pokemon_heights[j] + pedestal_heights[l] <= pokemon_heights[j - 1] + pedestal_heights[i - 1] or height_matrix[i - 1][j - 1] != m - k:
-                i -= 1
-            i -= 1
-            j -= 1
+height_matrix = [[0 for j in range(n)] for i in range(m)]
+height_matrix[0][0] = pedestal_heights[0] + pokemon_heights[0]
+max_value = height_matrix[0][0]
+for i in range(1, n):
+    if pedestal_heights[i] + pokemon_heights[0] < max_value:
+        height_matrix[0][i], max_value = pedestal_heights[i] + pokemon_heights[0], pedestal_heights[i] + pokemon_heights[0]
+    else:
+        height_matrix[0][i] = max_value
+for i in range(1, m):
+    max_value = float("inf")
+    for j in range(i, n):
+        if height_matrix[i-1][j-1] == -1:
+            height_matrix[i][j] = -1
+            continue
+        if height_matrix[i - 1][j - 1] < pokemon_heights[i] + pedestal_heights[j] and pokemon_heights[i] + pedestal_heights[j] < max_value:
+            max_value = pokemon_heights[i] + pedestal_heights[j]
+            height_matrix[i][j] = max_value
+        elif height_matrix[i - 1][j - 1] < pokemon_heights[i] + pedestal_heights[j]:
+            height_matrix[i][j] = max_value
+        elif i != j and height_matrix[i - 1][j - 1] > pokemon_heights[i] + pedestal_heights[j]:
+            height_matrix[i][j] = max_value
         else:
-            i -= 1
-    print(*sorted(positions))
+            height_matrix[i][j] = -1
+i = m - 1
+j = n - 1
+positions = []
+while 0 <= i <= j:
+    if height_matrix[i][j] == pokemon_heights[i] + pedestal_heights[j]:
+        positions.append(j)
+        i -= 1
+        j -= 1
+    else:
+        j -= 1
+if len(positions) == m:
+    for i in sorted(positions):
+        print(i + 1, end=' ')
+else:
+    print(-1)
